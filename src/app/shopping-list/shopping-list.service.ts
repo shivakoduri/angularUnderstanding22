974a92b711dfe250c0fb39e08d31c7d6e2bca2fc
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-05-20 09:11:50 +0530

    commit for Cross-Component communication

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,18 +1,22 @@
 import {Ingredient} from "../shared/Ingredient.model";
+import {EventEmitter} from "@angular/core";
 
 export class ShoppingListService {
 
-  private  ingredients: Ingredient[]= [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ];
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
 
-  getIngredients(){
-    return this.ingredients.slice();
+  private  ingredients: Ingredient[]= [
+      new Ingredient('Apples', 5),
+      new Ingredient('Tomatoes', 10),
+    ];
+
+    getIngredients(){
+      return this.ingredients.slice();
   }
 
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
   }
 
 
