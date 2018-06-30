10a25bcd0ebaf69a2b435baee7ea122678f52fff
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-06-30 22:03:19 +0530

    Handling Form with template driven approach

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import {Subject} from 'rxjs/Subject';
 export class ShoppingListService {
 
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private  ingredients: Ingredient[]= [
       new Ingredient('Apples', 5),
@@ -12,6 +13,10 @@ export class ShoppingListService {
 
     getIngredients(){
       return this.ingredients.slice();
+  }
+
+  getIngredient(index: number){
+      return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient){
@@ -27,5 +32,13 @@ export class ShoppingListService {
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 
+  updateIngredient(index: number, newIngredient: Ingredient){
+      this.ingredients[index] = newIngredient;
+      this.ingredientsChanged.next(this.ingredients.slice());
+  }
 
+  deleteIngedient(index: number){
+      this.ingredients.splice(index, 1);
+      this.ingredientsChanged.next(this.ingredients.slice());
+  }
 }
